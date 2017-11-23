716a5a9c4bac9838020e1105c5c79606cc9bbe2b
Michal <Michal@email.com>
2017-11-23 16:27:46 +0000

    FIX: adding depenedency on typing module so it works with python < 3.5

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         ]
     },
     install_requires=[
+        'typing'
     ],
     keywords='REST HATEOAS Hypermedia RESTful SIREN HAL API JSONAPI web framework Django Flask SQLAlchemy Cassandra',
     name='pysiren',
